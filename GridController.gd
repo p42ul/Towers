bda9a16859ee3ab5218d5d2c80a94a4a08a52471
Paul Buser <buser.paul@gmail.com>
2022-12-20 21:48:30 -0500

    Pathfinding is *almost* working!

--- GridController.gd
+++ GridController.gd
@@ -6,21 +6,54 @@ var height = 6
 var grid_node_size = 40
 
 var grid = []
+var aStar
 
 export(PackedScene) var grid_node
 
-signal create_tower(x, y)
-signal remove_tower(x, y)
-
 func _ready():
+	aStar = AStar2D.new()
+	aStar.reserve_space(width*height)
 	grid.resize(width)
 	grid.fill([])
 	for x in range(width):
 		grid[x].resize(height)
 		for y in range(height):
 			var node = grid_node.instance()
-			grid[x][y] = node
+			var nodeId = _get_node_id(x, y)
+			aStar.add_point(nodeId, Vector2(x, y))
+			if x > 0:
+				aStar.connect_points(nodeId, _get_node_id(x-1, y))
+			if y > 0:
+				aStar.connect_points(nodeId, _get_node_id(x, y-1))
 			node.position.x = x*grid_node_size
 			node.position.y = y*grid_node_size
+			node.x = x
+			node.y = y
 			add_child(node)
+			grid[x][y] = node
 			node.set_size(grid_node_size)
+			node.connect("tower_created", self, "_on_tower_created")
+			node.connect("tower_removed", self, "_on_tower_removed")
+			
+
+func _get_node_id(x, y):
+	return y*width + x
+
+func _on_tower_created(x, y):
+	var nodeId = _get_node_id(x, y)
+	aStar.set_point_disabled(nodeId, true)
+	print("tower created: " + str(x) + ", " + str(y))
+	
+func _on_tower_removed(x, y):
+	var nodeId = _get_node_id(x, y)
+	aStar.set_point_disabled(nodeId, false)
+	print("tower removed: " + str(x) + ", " + str(y))
+
+func _on_find_path():
+	print("finding path")
+	var path = aStar.get_point_path(0, width*height-1)
+	for grid_point in path:
+		print(grid_point)
+		var x = grid_point.x
+		var y = grid_point.y
+		grid[x][y].flash_color(Color.aquamarine)
--- GridNode.gd
+++ GridNode.gd
@@ -1,13 +1,19 @@
 extends Area2D
 
+var x
+var y
+var last_color
 var has_tower = false
+
+signal tower_created(x, y)
+signal tower_removed(x, y)
 
 func _input_event(viewport, event, shape_idx):
 	if Input.is_action_pressed("grid_draw"):
 		create_tower()
 	elif Input.is_action_pressed("grid_remove"):
 		remove_tower()
-		
+
 func set_size(r: int):
 	$ColorRect.rect_size = Vector2(r, r)
 	$ColorRect.margin_right = r
@@ -20,7 +26,24 @@ func set_size(r: int):
 	$CollisionShape2D.scale.y = r/2
 
 func create_tower():
+	if has_tower:
+		return
 	$ColorRect.color = Color.rosybrown
+	emit_signal("tower_created", x, y)
+	has_tower = true
+	
 	
 func remove_tower():
+	if not has_tower:
+		return
 	$ColorRect.color = Color.white
+	emit_signal("tower_removed", x, y)
+	has_tower = false
+
+func flash_color(color):
+	last_color = $ColorRect.color
+	$ColorRect.color = color
+	$ColorResetTimer.start()
+
+func _on_color_reset_color():
+	$ColorRect.color = last_color

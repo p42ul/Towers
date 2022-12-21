28692b7cbd65d57499e96169969149918a39ae53
Paul Buser <buser.paul@gmail.com>
2022-12-20 20:42:46 -0500

    Add ability to resize grid

--- GridController.gd
+++ GridController.gd
@@ -3,17 +3,14 @@ extends Node
 var width = 12
 var height = 6
 
+var grid_node_size = 40
+
 var grid = []
 
 export(PackedScene) var grid_node
 
 signal create_tower(x, y)
 signal remove_tower(x, y)
-
-func _input(event):
-	if Input.is_action_just_pressed("grid_click"):
-		var mouse_position = get_viewport().get_mouse_position()
-		print("CLICK!" + str(mouse_position))
 
 func _ready():
 	grid.resize(width)
@@ -23,8 +20,7 @@ func _ready():
 		for y in range(height):
 			var node = grid_node.instance()
 			grid[x][y] = node
-			node.position.x = x*40
-			node.position.y = y*40
-			node.x = x
-			node.y = y
+			node.position.x = x*grid_node_size
+			node.position.y = y*grid_node_size
 			add_child(node)
+			node.set_size(grid_node_size)
--- GridNode.gd
+++ GridNode.gd
@@ -1,4 +1,26 @@
-extends Node2D
+extends Area2D
 
-var x
-var y
+var has_tower = false
+
+func _input_event(viewport, event, shape_idx):
+	if Input.is_action_pressed("grid_draw"):
+		create_tower()
+	elif Input.is_action_pressed("grid_remove"):
+		remove_tower()
+		
+func set_size(r: int):
+	$ColorRect.rect_size = Vector2(r, r)
+	$ColorRect.margin_right = r
+	$ColorRect.margin_bottom = r
+	$ReferenceRect.margin_right = r-1
+	$ReferenceRect.margin_bottom = r-1
+	$CollisionShape2D.position.x = r/2
+	$CollisionShape2D.position.y = r/2
+	$CollisionShape2D.scale.x = r/2
+	$CollisionShape2D.scale.y = r/2
+
+func create_tower():
+	$ColorRect.color = Color.rosybrown
+	
+func remove_tower():
+	$ColorRect.color = Color.white

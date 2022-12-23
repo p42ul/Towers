17e6fef4d85316d5d63f07eb4e77f399a3a17ca8
Paul Buser <buser.paul@gmail.com>
2022-12-22 21:52:19 -0500

    Automatic pathfinding.

--- tower_grid/GridController.gd
+++ tower_grid/GridController.gd
@@ -3,7 +3,10 @@ extends Node2D
 var width = 24
 var height = 12
 
-var grid_node_size = 40
+var start_grid_id = width*height + 1
+var end_grid_id = width*height + 2
+
+var grid_node_size = 32
 
 var grid = []
 var aStar
@@ -16,14 +19,20 @@ export(PackedScene) var grid_node
 func _ready():
 	follower = $MobPath/MobPathFollower
 	aStar = AStar2D.new()
-	aStar.reserve_space(width*height)
+	aStar.reserve_space(width*height+2)
 	grid.resize(width*height)
+	aStar.add_point(start_grid_id, Vector2(-grid_node_size, height/2))
+	aStar.add_point(end_grid_id, Vector2(width*grid_node_size+grid_node_size, height/2))
 	for x in range(width):
 		for y in range(height):
 			var node = grid_node.instance()
 			var nodeId = _get_node_id(x, y)
 			grid[nodeId] = node
 			aStar.add_point(nodeId, Vector2(x, y))
+			if x == 0:
+				aStar.connect_points(start_grid_id, nodeId)
+			if x == width-1:
+				aStar.connect_points(end_grid_id, nodeId)
 			if x > 0:
 				aStar.connect_points(nodeId, _get_node_id(x-1, y))
 			if y > 0:
@@ -36,7 +45,7 @@ func _ready():
 			node.set_size(grid_node_size)
 			node.connect("tower_created", self, "_on_tower_created")
 			node.connect("tower_removed", self, "_on_tower_removed")
-			
+	recalculate_path()
 
 func _get_node_id(x, y):
 	return y*width + x
@@ -52,18 +61,20 @@ func _on_tower_removed(x, y):
 	recalculate_path()
 
 func recalculate_path():
-	var path = aStar.get_id_path(0, width*height-1)
+	var path = aStar.get_id_path(start_grid_id, end_grid_id)
 	if path.size() < 2:
 		valid_path = false
 		return
 	valid_path = true
 	var curve = $MobPath.get_curve()
 	curve.clear_points()
-	for grid_id in path:
+	# Strip the first and last path entry, which aren't in the grid
+	for i in range(1, path.size() - 1):
+		var grid_id = path[i]
 		var x = grid[grid_id].position.x + grid_node_size / 2
 		var y = grid[grid_id].position.y + grid_node_size / 2
 		curve.add_point(Vector2(x, y))
 
 func _process(delta):
 	if valid_path:
-		follower.unit_offset += delta / 10
+		follower.offset += delta * 100
